0efe2004aef008bea0fc92d456ca68ef74cffdc1
Nicolas Souza <nj776640@gmail.com>
2025-11-29 00:42:08 -0300

    criação do cabeçalho da pagina e de estilizações globais

---
+++ src/styles.ts
@@ -0,0 +1,30 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const cores = {
+  branco: '#eee',
+  preto: '#111',
+  cinza: '#333',
+  verde: '#10ac84'
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
+    text-decoration: none;
+  }
+
+  body {
+    background-color: ${cores.preto};
+    color: ${cores.branco};
+    padding-top: 40px;
+  }
+`
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+`
